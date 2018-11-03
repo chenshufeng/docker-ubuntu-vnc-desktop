9bf69178de77b599e2d656f4ca703c8697582e2e
zhouzhipeng <admin@zhouzhipeng.com>
2018-11-03 15:48:57 +0800

    安装intellij idea 及基本设置

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ docker run --name newcc -d -p 3589:3389  -p 5902:5900 -p 6180:80\
 docker logs -f newcc
 # -v /data/root_data:/root \
 
-# registry.mob.com/zhouzhipeng/docker-ubuntu-xrdp-lxqt:avalable  基本可用版本 (包含vnc,novnc,xrdp,lxqt)
+# registry.mob.com/zhouzhipeng/docker-ubuntu-xrdp-lxqt:avalable  基本可用版本 (包含vnc,novnc,xrdp,lxqt,chrome,有puslaudio 但是没有声音)
 
 
 ## pulseaudio  modules :   /usr/lib/pulse-11.1/modules
